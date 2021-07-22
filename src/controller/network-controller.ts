c9384bd43a9b0dc5201a3dc6ed8ca3721eb38e1a
qiuwenju <qiuwenju@haier.com>
2021-07-22 18:28:05 +0800

    test:来了老弟

--- src/controller/network-controller.ts
+++ src/controller/network-controller.ts
@@ -1,19 +1,27 @@
-import * as Koa from 'koa'
-import NetWorkService from '../service/network-service'
+import * as Koa from "koa";
+import NetWorkService from "../service/network-service";
 
 abstract class NetWorkContollerAbstract {
-  public name = '1';
+  public name = "1";
   public constructor() {
-    console.log('init-------abstract')
+    console.log("init-------abstract");
   }
-  public abstract createNetwork(ctx: Koa.ParameterizedContext, next: Koa.Next): void;
+  public abstract createNetwork(
+    ctx: Koa.ParameterizedContext,
+    next: Koa.Next
+  ): void;
   public abstract getList(ctx: Koa.Context, next: Koa.Next): void;
   public abstract getListChart(ctx: Koa.Context, next: Koa.Next): void;
 }
-
+var a = 1
+console.log(a)
 class NetWorkContoller extends NetWorkContollerAbstract {
-  public constructor () {
-    super()
+  public constructor() {
+    super();
+    
+
+
+    
   }
   // public 修饰的属性或方法是公有的，可以在任何地方被访问到，默认所有的属性和方法都是 public 的
   // private 修饰的属性或方法是私有的，不能在声明它的类的外部访问
@@ -23,9 +31,12 @@ class NetWorkContoller extends NetWorkContollerAbstract {
    * @param { ctx } Koa.ParameterizedContext Koa.Context extends Koa.ParameterizedContext
    * @param { next } Koa.Next
    */
-  public async createNetwork(ctx: Koa.ParameterizedContext, next: Koa.Next): Promise<void> {
-    const res: object = await NetWorkService.createNetwork(ctx, next)
-    ctx.body = res
+  public async createNetwork(
+    ctx: Koa.ParameterizedContext,
+    next: Koa.Next
+  ): Promise<void> {
+    const res: object = await NetWorkService.createNetwork(ctx, next);
+    ctx.body = res;
   }
   /**
    * name: getList
@@ -33,8 +44,8 @@ class NetWorkContoller extends NetWorkContollerAbstract {
    * @param { next } Koa.Next
    */
   public async getList(ctx: Koa.Context, next: Koa.Next): Promise<void> {
-    const res: object = await NetWorkService.getList(ctx, next)
-    ctx.body = res
+    const res: object = await NetWorkService.getList(ctx, next);
+    ctx.body = res;
   }
   /**
    * name: getListChart
@@ -42,9 +53,9 @@ class NetWorkContoller extends NetWorkContollerAbstract {
    * @param { next } Koa.Next
    */
   public async getListChart(ctx: Koa.Context, next: Koa.Next): Promise<void> {
-    const res: object = await NetWorkService.getListChart(ctx, next)
-    ctx.body = res
+    const res: object = await NetWorkService.getListChart(ctx, next);
+    ctx.body = res;
   }
 }
 
-export default new NetWorkContoller()
+export default new NetWorkContoller();
